9752aed6a58992f8f612e11dad9686e3c1b694ed
tkomatsu <tkomatsu@student.42tokyo.jp>
2021-11-10 08:22:33 +0900

    feat: intra login

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+/*
+ *https://utahta.hatenablog.com/blog/2016/08/16/Google_OAuth2_%E3%83%88%E3%83%BC%E3%82%AF%E3%83%B3%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B_with_Go
+ */
+
 import (
 	"context"
 	"fmt"
@@ -17,31 +21,39 @@ func main() {
 	if err := godotenv.Load(fmt.Sprintf("./%s.env", os.Getenv("GO_ENV"))); err != nil {
 		return
 	}
-	conf.ClientID = os.Getenv("CLIENT_ID")
-	conf.ClientSecret = os.Getenv("SECRET")
+	confGoogle.ClientID = os.Getenv("GOOGLE_CLIENT_ID")
+	confGoogle.ClientSecret = os.Getenv("GOOGLE_SECRET")
+	confIntra.ClientID = os.Getenv("INTRA_CLIENT_ID")
+	confIntra.ClientSecret = os.Getenv("INTRA_SECRET")
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/login", LoginHandler)
-	mux.HandleFunc("/loginr", LoginRHandler)
+	mux.HandleFunc("/login/google", GoogleLoginHandler)
+	mux.HandleFunc("/login/google/redirect", GoogleLoginRHandler)
+	mux.HandleFunc("/login/intra", IntraLoginHandler)
+	mux.HandleFunc("/login/intra/redirect", IntraLoginRHandler)
+
 	log.Println("Server has started")
+	fmt.Println("Pleas access: http://localhost:5001/login/google")
+	fmt.Println("Pleas access: http://localhost:5001/login/intra")
 	http.ListenAndServe(":5001", mux)
 }
 
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	var url = conf.AuthCodeURL("yourStateUUID", oauth2.AccessTypeOffline)
-	fmt.Fprintf(w, "Visit here : %s", url)
+func GoogleLoginHandler(w http.ResponseWriter, r *http.Request) {
+	var url = confGoogle.AuthCodeURL("yourStateUUID", oauth2.AccessTypeOffline)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
-func LoginRHandler(w http.ResponseWriter, r *http.Request) {
+func GoogleLoginRHandler(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query()["code"]
 	if code == nil || len(code) == 0 {
 		fmt.Fprint(w, "Invalid Parameter")
 	}
 	ctx := context.Background()
-	tok, err := conf.Exchange(ctx, code[0])
+	tok, err := confGoogle.Exchange(ctx, code[0])
 	if err != nil {
 		fmt.Fprintf(w, "OAuth Error:%v", err)
 	}
-	client := conf.Client(ctx, tok)
+	client := confGoogle.Client(ctx, tok)
 	svr, err := oauthapi.New(client)
 	ui, err := svr.Userinfo.Get().Do()
 	if err != nil {
@@ -51,10 +63,50 @@ func LoginRHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-var conf = &oauth2.Config{
+var confGoogle = &oauth2.Config{
 	ClientID:     "",
 	ClientSecret: "",
 	Scopes:       []string{oauthapi.UserinfoEmailScope},
 	Endpoint:     google.Endpoint,
-	RedirectURL:  "http://localhost:5001/loginr",
+	RedirectURL:  "http://localhost:5001/login/google/redirect",
+}
+
+func IntraLoginHandler(w http.ResponseWriter, r *http.Request) {
+	var url = confIntra.AuthCodeURL("")
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func IntraLoginRHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("IntraLoginRHandler")
+	code := r.URL.Query()["code"]
+	if code == nil || len(code) == 0 {
+		fmt.Fprint(w, "Invalid Parameter")
+	}
+	log.Println("Code is valid")
+	ctx := context.Background()
+	tok, err := confIntra.Exchange(ctx, code[0])
+	if err != nil {
+		fmt.Fprintf(w, "OAuth Error:%v", err)
+	}
+	log.Println("Token exchange success")
+	client := confIntra.Client(ctx, tok)
+	res, err := client.Get("https://api.intra.42.fr/v2/me/projects")
+	if err != nil {
+		log.Println("/me/projects failed")
+		fmt.Fprintln(w, "Error: ", err)
+	} else {
+		log.Println("/me/projects SUCCEEDED!!!!!!!!")
+		fmt.Fprintln(w, res.Body)
+	}
+}
+
+var confIntra = &oauth2.Config{
+	ClientID:     "",
+	ClientSecret: "",
+	Scopes:       []string{"public", "projects", "profile", "elearning", "tig", "forum"},
+	Endpoint: oauth2.Endpoint{
+		AuthURL:  "https://api.intra.42.fr/oauth/authorize",
+		TokenURL: "https://api.intra.42.fr/oauth/token",
+	},
+	RedirectURL: "http://localhost:5001/login/intra/redirect",
 }
